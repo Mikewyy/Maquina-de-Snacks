9064da6e39652d0cc9b9515775eccc62c9120b75
Mike Grain <mikezgrain@gmail.com>
2026-03-09 12:41:28 -0500

    Creacion de clase snack

--- src/maquina_snacks/Snack.java
+++ src/maquina_snacks/Snack.java
@@ -1,4 +1,69 @@
 package maquina_snacks;
 
-public class Snack {
+import java.io.Serializable;
+import java.util.Objects;
+
+//implementamos javabeans
+public class Snack implements Serializable {
+    private static int contadorSnacks = 0;
+    private int idSnack;
+    private String nombre;
+    private double precio;
+
+    public Snack(){
+        //por cada objeto que se cree de tipo Snack se aumentara el id
+        this.idSnack = ++Snack.contadorSnacks;
+    }
+
+    public Snack(String nombre, double precio){
+        this();//Debe ser la primer linea. Manda a llamar al constructor vacio para inicializar idSnack
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public static int getContadorSnacks() {
+        return contadorSnacks;
+    }
+
+    public int getIdSnack() {
+        return idSnack;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Snack{" +
+                "idSnack=" + idSnack +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+
+    //generamos metodo equals y hashCode
+    @Override
+    public boolean equals(Object o) {//Realiza comparaciones de contenido
+        if (o == null || getClass() != o.getClass()) return false;
+        Snack snack = (Snack) o;
+        return idSnack == snack.idSnack && Double.compare(precio, snack.precio) == 0 && Objects.equals(nombre, snack.nombre);
+    }
+
+    @Override
+    public int hashCode() {//Realiza comparaciones de tipo numerico
+        return Objects.hash(idSnack, nombre, precio);
+    }
 }
